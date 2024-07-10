916fbf8fa64e62fa7e801b8c5a750c0de6c2b21a
simonagrassi <simonavgrassi@g>
2024-07-10 12:49:08 +0100

    Set up skeleton for contest

--- contest/__init__.py
+++ contest/__init__.py
@@ -9,19 +9,40 @@ A simple Tullock
 class C(BaseConstants):
     NAME_IN_URL = 'contest'
     PLAYERS_PER_GROUP = None
-    NUM_ROUNDS = 1
+    NUM_ROUNDS = 2
+    ENDOWMENT = 20
+    COST_PER_TICKET = 1
 
 
 class Subsession(BaseSubsession):
-    pass
+    is_paid = models.BooleanField()
 
+    def setup(self):
+        self.is_paid = (self.round_number==1)
+        for group in self.get_groups():
+            group.setup()
 
 class Group(BaseGroup):
-    pass
+    def setup(self):
+        for player in self.get_players():
+            player.setup()
 
 
 class Player(BasePlayer):
-    pass
+    endowment = models.IntegerField()
+    cost_per_ticket = models.IntegerField()
+    tickets_purchased = models.IntegerField()
+    is_winner = models.BooleanField()
+    earnings = models.IntegerField()
+
+    def setup(self):
+        self.endowment = C.ENDOWMENT
+        self.cost_per_ticket = C.COST_PER_TICKET
+
+
+# def creating_session(subsession):
+ #   subsession.setup()
+
 
 
 # PAGES
@@ -30,7 +51,10 @@ class Intro(Page):
 
 
 class SetupRound(WaitPage):
-    pass
+    wait_for_all_groups = True
+    @staticmethod
+    def after_all_players_arrive(subsession):
+        subsession.setup()
 
 
 class Decision(Page):
